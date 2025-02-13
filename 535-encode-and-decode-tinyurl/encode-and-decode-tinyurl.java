4b6146c0ee2d75063778659ed45fad71e2d048d5
Vasantha Kumar .V <143257001+VasanthaKumar-20@users.noreply.github.com>
2025-02-13 13:36:55 +0530

    Time: 16 ms (5.08%) | Memory: 45.8 MB (5.45%) - LeetSync

--- 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.java
@@ -2,12 +2,32 @@ public class Codec {
 
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        return longUrl;
+        int n = longUrl.length();
+        String ans="";
+        for(int i=0;i<n;i++)
+        {
+            char ch = longUrl.charAt(i);
+            int d = ch+1;
+            char ch1 =(char) d;
+            ans+=ch1; 
+        }
+        System.out.println(ans);
+        return ans;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
+        int n = shortUrl.length();
+        String ans="";
+        for(int i=0;i<n;i++)
+        {
+            char ch = shortUrl.charAt(i);
+            int d = ch-1;
+            char ch1 =(char) d;
+            ans+=ch1; 
+        }
+        System.out.println(ans);
+        return ans;
     }
 }
 
