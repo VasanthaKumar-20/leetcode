c687ad59d4b23b56c28586ef4fb64a2790f76008
Vasantha Kumar .V <143257001+VasanthaKumar-20@users.noreply.github.com>
2025-07-02 15:33:59 +0530

    Time: 3 ms (0.36%) | Memory: 41.9 MB (9.70%) - LeetSync

---
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int sum=0;
+    public boolean checkTree(TreeNode root) {
+        func(root);
+        System.out.println(sum);
+        return (root.val==sum-root.val);
+    }
+    public void func(TreeNode node)
+    {
+        if(node==null)
+        {
+            return;
+        }
+        sum+=node.val;
+        func(node.left);
+        func(node.right);
+    }
+}
